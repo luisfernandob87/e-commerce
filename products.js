43988ed23d2010a0539e6cc6825ad11600f91c46
luisfernandob87 <luisfernandob87@gmail.com>
2022-06-11 01:19:45 -0600

    avances 10062022

---
+++ products.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let products = [
+    { id: 1, imagen: 'playera_azul_version2.webp', nombre: 'Playera Azul',  precio: 100, stock: 5},
+    { id: 2, imagen: 'playera_blanca_version2.webp', nombre: 'Playera Blanca',  precio: 80, stock: 5},
+    { id: 3, imagen: 'playera_corinta_version2.webp', nombre: 'Playera Corinta',  precio: 110, stock: 5},
+    { id: 4, imagen: 'playera_gris_version2.webp', nombre: 'Playera Gris',  precio: 75, stock: 5},
+    { id: 5, imagen: 'playera_negra.webp', nombre: 'Playera Negra',  precio: 125, stock: 5},
+    { id: 6, imagen: 'sudadero_azul_version2.webp', nombre: 'Sudadero Azul',  precio: 250, stock: 5},
+    { id: 7, imagen: 'sudadero_negro_version2.webp', nombre: 'Sudadero Negro',  precio: 275, stock: 5},
+    { id: 8, imagen: 'sudadero_gris.webp', nombre: 'Sudadero Gris',  precio: 280, stock: 5},
+    { id: 9, imagen: 'sueter_azul.webp', nombre: 'Sueter Azul',  precio: 290, stock: 5},
+    { id: 10, imagen: 'sueter_negro.webp', nombre: 'Sueter Negro',  precio: 300, stock: 5},
+];
+
+function generateCard(products) {
+    const container = document.getElementById('container')
+
+    let html = ''
+    for (let i = 0; i < products.length; i++) {
+        html += `<div class="container-item">
+                        <img src="/img/${products[i].imagen}" alt="Imagen de producto">
+                        <p>${products[i].nombre}</p>
+                        <p>$${products[i].precio}</p>
+                        <button>Agregar al carrito</button>
+                      </div>`;       
+    }
+    container.innerHTML += html;
+}
+
+generateCard(products);
