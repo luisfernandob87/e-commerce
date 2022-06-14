a8da28fd07e2950bc9b24ad41ce36ee3462827ee
luisfernandob87 <luisfernandob87@gmail.com>
2022-06-14 11:49:26 -0600

    cambios 13/06/2022

--- cart.js
+++ cart.js
@@ -10,15 +10,40 @@ if (shoppingCart){
 function generateCardCart(shoppingCart) {
   const container = document.getElementById("container-cart");
 
-  let html = "";
+  let html = '';
   for (let j = 0; j < shoppingCart.length; j++) {
     html += `<div class="container-item">
                         <img src="/img/${shoppingCart[j].imagen}" alt="Imagen de producto">
                         <p>${shoppingCart[j].nombre}</p>
                         <p>$${shoppingCart[j].precio}</p>
+                        <button onclick="removeCart(${shoppingCart[j].id})")>X</button>
                       </div>`;
   }
-  container.innerHTML = html;
+  container.innerHTML += html;
 }
 generateCardCart(shoppingCart);
 
+
+function find(array, cb){
+  for(let j=0; j<array.length; j++){
+    let result = cb(array[j]);
+    if(result===true){
+      return array[j];
+    }
+  }
+}
+
+function removeCart(id) {
+  function cbFindById(shoppingCart){
+    return shoppingCart.id === id;
+  }
+  let product = find(shoppingCart, cbFindById)
+  console.log(product);
+  list = JSON.parse(product);
+  // product.splice(product, 0)
+  console.log(list);
+  //  localStorage.removeItem('shoppingCart',JSON.stringify(shoppingCart))
+}
+//  generateCardCart(shoppingCart);
+
+// window.removeCart = removeCart;
