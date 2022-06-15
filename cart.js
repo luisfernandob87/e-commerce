ddc0f634c9e480092e98e2b34cb1cbf6bf8df19e
luisfernandob87 <luisfernandob87@gmail.com>
2022-06-15 00:10:11 -0600

    cambios 15/06/2022

--- cart.js
+++ cart.js
@@ -6,24 +6,32 @@ if (shoppingCart){
 }else{
   shoppingCart = [];
 }
-
 function generateCardCart(shoppingCart) {
   const container = document.getElementById("container-cart");
-
   let html = '';
   for (let j = 0; j < shoppingCart.length; j++) {
     html += `<div class="container-item">
                         <img src="/img/${shoppingCart[j].imagen}" alt="Imagen de producto">
                         <p>${shoppingCart[j].nombre}</p>
                         <p>$${shoppingCart[j].precio}</p>
-                        <button onclick="removeCart(${shoppingCart[j].id})">X</button>
+                        <button class="remove" onclick="removeCart(${shoppingCart[j].id})">X</button>
                       </div>`;
   }
   container.innerHTML += html;
 }
 generateCardCart(shoppingCart);
+  let pagoTotal = 0;
+for (let l = 0; l < shoppingCart.length; l++) {
+  let pago = shoppingCart[l].precio;
+  pagoTotal += pago;
+}
 
-
+function totalPago(pagoTotal){
+  let containerPago = document.getElementById("total");
+  let htmlPago = `<p>$${pagoTotal}</p><button onclick="finalizar()" class="finalizar">Finalizar Compra</button>`;
+  containerPago.innerHTML = htmlPago;
+}
+totalPago(pagoTotal);
 function find(array, cb){
   for(let j=0; j<array.length; j++){
     let result = cb(array[j]);
@@ -32,20 +40,22 @@ function find(array, cb){
     }
   }
 }
-
-function removeCart(id) {
-
-
-  function cbFindById(shoppingCart){
-    return shoppingCart.id === id;
+function removeCart(id) { 
+  let shoppingTemp = JSON.parse(localStorage.getItem('shoppingCart'));
+  function cbFindById(shoppingTemp){
+    return shoppingTemp.id === id;
   }
-  let product = find(shoppingCart, cbFindById);
-  console.log(product);
-
-  
-  let del = JSON.parse(localStorage.getItem('shoppingCart'))
-  console.log(del);
-  del.splice(product.id,1);
-  localStorage.setItem('shoppingCart', JSON.stringify(del));
-  // window.location.reload()
+  let product = find(shoppingTemp, cbFindById);
+  for (let k = 0; k < shoppingTemp.length; k++) {
+      if (product.id === shoppingTemp[k].id) {
+        shoppingTemp.splice(k,1)
+      }
+    }
+  localStorage.setItem('shoppingCart', JSON.stringify(shoppingTemp));
+  // generateCardCart(shoppingTemp);
+  window.location.reload();
+}
+function finalizar(){
+  localStorage.clear();
+  window.location.reload();
 }
